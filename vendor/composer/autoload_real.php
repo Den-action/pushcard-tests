0c4fd0339f04a67bcc38a92044fef08f6e5ff65a
Denis Tikhonov <admkng5@mail.ru>
2017-01-13 19:05:33 +0200

    Add new vendors

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit38a5bb113d0526715a41bdc91b5336e4
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit38a5bb113d0526715a41bdc91b5336e4::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire38a5bb113d0526715a41bdc91b5336e4($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire38a5bb113d0526715a41bdc91b5336e4($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
