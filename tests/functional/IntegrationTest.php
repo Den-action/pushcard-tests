d48f6705f5f4c470e8afe0055c487339ce4ade7c
Denis Tikhonov <admkng5@mail.ru>
2017-01-27 16:05:52 +0200

    Fixed IntegrationTest config

--- tests/functional/IntegrationTest.php
+++ tests/functional/IntegrationTest.php
@@ -1,5 +1,5 @@
 <?php
-$I = new FuctionalTester($scenario);
+$I = new FunctionalTester($scenario);
 $I->wantTo('Check request from site');
 $I->am('user');
 $data=['platform'=>'ios','udid'=>'550e8400-e29b-41d4-a716-446655440000'];
