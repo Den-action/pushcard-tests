f59061804c7e32db2f50204e9e974116959005f3
Denis Tikhonov <admkng5@mail.ru>
2017-02-01 19:26:18 +0200

    Add JsonSchema file

--- tests/acceptance/IntegrationCept.php
+++ tests/acceptance/IntegrationCept.php
@@ -7,7 +7,7 @@ $data=['platform'=>'ios','udid'=>'550e8400-e29b-41d4-a716-446655440000'];
 $I->sendPOST('v1/token',$data);
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-#$I->canSeeResponseIsValidOnSchemaFile($I->getSchemaFileName('Error'));
+$I->canSeeResponseIsValidOnSchemaFile('/var/www/pushcard-tests/JsonSchema.json');
 #$I->canSeeResponseContainsJson(['err' => ['TOKEN_REQUIRED']]);
 $I->seeResponseMatchesJsonType(['token' => 'string']);
 ?>
